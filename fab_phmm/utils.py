90a6f66ed22540d0f806747a24a5ee64a5e5d21f
seataK <297.1951@gmail.com>
2017-02-07 13:49:45 +0900

    score assessment utils

--- fab_phmm/utils.py
+++ fab_phmm/utils.py
@@ -27,3 +27,41 @@ def omit_gap(seq, gap_id=-1):
             out.append(a)
 
     return np.array(out)
+
+
+def tp(ans, pred):
+    return np.logical_and(ans, pred).sum()
+
+
+def tn(ans, pred):
+    return np.logical_and(np.logical_not(ans), np.logical_not(pred)).sum()
+
+
+def fp(ans, pred):
+    return np.logical_and(np.logical_not(ans), pred).sum()
+
+
+def fn(ans, pred):
+    return np.logical_and(ans, np.logical_not(pred)).sum()
+
+
+def gen_alignemnt_matrix(xseq_wgap, yseq_wgap):
+    assert(xseq_wgap.shape[0] == yseq_wgap.shape[0])
+    xlen = (xseq_wgap != -1).sum()
+    ylen = (yseq_wgap != -1).sum()
+    matrix = np.zeros((xlen + 1, ylen + 1), dtype=np.int_)
+
+    i, j = 0, 0
+    for x, y in zip(xseq_wgap, yseq_wgap):
+        if x != -1:
+            i += 1
+        if y != -1:
+            j += 1
+
+        if x!= -1 and y != -1:
+            matrix[i, j] = 1
+
+    assert (i, j) == (xlen, ylen)
+
+    return matrix
+
