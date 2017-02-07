7a60379909c74472b59e1f1d57dcd46edcdd4c44
seataK <297.1951@gmail.com>
2017-02-07 13:58:44 +0900

    changes in shrinkage operation (refactoring needed)

--- fab_phmm/fab_phmm.py
+++ fab_phmm/fab_phmm.py
@@ -5,9 +5,9 @@ import copy
 import timeit
 from concurrent.futures import ThreadPoolExecutor
 import threading
+import sys
 
 warnings.simplefilter("always")
-
 
 class Monitor:
     def __init__(self, threshold=1e-5):
@@ -51,7 +51,6 @@ class Monitor:
             return False
         else:
             return False
-
 
 def _incremental_search(xseqs, yseqs, n_match, n_xins, n_yins,
                         stop_threshold=1e-5, shrink_threshold=1e-2,
@@ -139,7 +138,7 @@ def decremental_greedy_selection(xseqs, yseqs,
     while True:
         model.fit(xseqs, yseqs, max_iter=max_iter, verbose=verbose, verbose_level=verbose_level, n_threads=n_threads)
         models.append(copy.deepcopy(model))
-        if not model.greedy_shrink():
+        if not model.greedy_shrink(xseqs, yseqs):
             break
 
     if sorted:
@@ -154,7 +153,7 @@ class FABPHMM(PHMM):
                  n_simbols=4, initprob=None, transprob=None, emitprob=None,
                  symmetric_emission=False, shrink_threshold=1e-2,
                  stop_threshold=1e-5, link_hstates=False, shrink=True,
-                 propdim_count_nonzero=True):
+                 propdim_count_nonzero=True, link_match=True):
 
         super(FABPHMM, self).__init__(n_match_states=n_match_states,
                                       n_xins_states=n_xins_states,
@@ -164,7 +163,8 @@ class FABPHMM(PHMM):
                                       transprob=transprob,
                                       emitprob=emitprob,
                                       stop_threshold=stop_threshold,
-                                      link_hstates=link_hstates)
+                                      link_hstates=link_hstates,
+                                      link_match=link_match)
         # implement symmetric one for the first program
 
         self._symmetric_emission = symmetric_emission
@@ -190,89 +190,90 @@ class FABPHMM(PHMM):
             self._dim_match_emit = n_simbols ** 2 - 1
             self._dim_ins_emit = n_simbols - 1
 
-
     def _gen_dims(self):
         # assume symmetric output prob disribution
         n_hstates = self._n_hstates
         dim_emit = np.zeros(n_hstates)
+        dim_trans = np.sum(self._transprob != 0, axis=1) - 1
 
-        if self._propdim_count_nonzero:
-            dim_trans = np.sum(self._transprob != 0, axis=1) - 1
-            dim_init = np.sum(self._initprob != 0) - 1
-        else:
-            dim_trans = np.ones(n_hstates) * (n_hstates - 1)
-            dim_init = n_hstates - 1
+        dim_init = np.sum(self._initprob != 0) - 1
 
         for k in range(n_hstates):
             hstate_prop = self._hstate_properties[k]
             dim_emit[k] = self._dim_match_emit if hstate_prop == 0 else self._dim_ins_emit
         return dim_init, dim_trans, dim_emit
 
-
-    def _gen_pseudo_prob(self, log_emitprob_frame, dims_trans, dims_emit):
+    def _gen_logdelta(self, log_emitprob_frame, dims_trans, dims_emit):
         term_trans = - dims_trans / (2 * self._qsum_trans + EPS)
         term_emit = - dims_emit / (2 * self._qsum_emit + EPS)
 
-        score_1 = np.exp(term_trans + term_emit)
-        norm_1 = np.sum(score_1)
-        score_2 = np.exp(term_emit)
-        norm_2 = np.sum(score_2)
+        logdelta_1 = term_trans + term_emit
+        logdelta_2 = term_emit
 
         xshape, yshape, n_hstates = log_emitprob_frame.shape
 
-        norm = np.ones((xshape, yshape)) * norm_1
-        norm[-1, -1] = norm_2
+        pseudo_prob = np.full((xshape, yshape, n_hstates), logdelta_1)
+        pseudo_prob[-1, -1, :] = logdelta_2
 
-        pseudo_prob = np.ones((xshape, yshape, n_hstates)) * (score_1 / norm_1)[np.newaxis, np.newaxis, :]
-        pseudo_prob[-1, -1, :] = score_2 / norm_2
+        return pseudo_prob
 
-        return norm, pseudo_prob
+    def _get_state_to_delete(self):
+        if self._n_match_states == 1 and self._n_xins_states == 1 and self._n_yins_states == 1:
+            return -1, -1
 
-    def _shrinkage_operation(self, dry=False):
-        below_threshold = (self._qsum_emit / np.sum(self._qsum_emit)) < (self._shrink_threshold)
-        preserved_hstates, = np.nonzero(~below_threshold)
-        deleted_hstates, = np.nonzero(below_threshold)
+        dim_init, dims_trans, dims_emit = self._gen_dims()
+        qscore = self._qsum_emit / (dims_trans + dims_emit)
+        qscore /= np.sum(qscore)
 
-        n_deleted_hstateprops = np.zeros(3, dtype=np.int)
-        for k in deleted_hstates:
-            n_deleted_hstateprops[self._hstate_properties[k]] += 1
+        sep_mx = self._n_match_states
+        sep_xy = self._n_match_states + self._n_xins_states
 
-        if np.sum(n_deleted_hstateprops) == 0:
+        qscore_match = qscore[:sep_mx]
+        qscore_xins = qscore[sep_mx:sep_xy]
+        qscore_yins = qscore[sep_xy:]
+
+        def min_val_index(qsum, origin):
+            argmin = np.argmin(qsum)
+            min_val = qsum[argmin]
+            min_index = argmin + origin
+            return min_val, min_index
+
+        cands = []
+        if self._n_match_states > 1:
+            cands.append(min_val_index(qscore_match, 0))
+        if self._n_xins_states > 1:
+            cands.append(min_val_index(qscore_xins, sep_mx))
+        if self._n_yins_states > 1:
+            cands.append(min_val_index(qscore_yins, sep_xy))
+
+        val, state = min(cands, key=lambda k: k[0])
+
+        return val, state
+
+    def _shrinkage_operation(self, force=False):
+        val, state = self._get_state_to_delete()
+
+        if state == -1:
             return 0
 
-        self._last_shrinked = True
+        if force:
+            self._delete_state(state)
+            return 1
 
-        if dry:
-            return np.sum(n_deleted_hstateprops)
+        n_deleted = 0
+        while val < self._shrink_threshold:
+            self._delete_state(state)
+            n_deleted += 1
+            val, state = self._get_state_to_delete()
+            if state == -1:
+                break
 
-        self._n_match_states -= n_deleted_hstateprops[0]
-        self._n_xins_states -= n_deleted_hstateprops[1]
-        self._n_yins_states -= n_deleted_hstateprops[2]
-        assert(self._n_match_states > 0)
-        assert(self._n_xins_states > 0)
-        assert(self._n_yins_states > 0)
-
-        self._n_hstates -= np.sum(n_deleted_hstateprops)
-
-        self._hstate_properties = self._gen_hstate_properties()
-
-        self._initprob = self._initprob[preserved_hstates]
-        self._transprob = self._transprob[:, preserved_hstates]
-        self._transprob = self._transprob[preserved_hstates, :]
-        self._emitprob = self._emitprob[preserved_hstates, :, :]
-        self._qsum_emit = self._qsum_emit[preserved_hstates]
-        self._qsum_trans = self._qsum_trans[preserved_hstates]
-
-        for k in deleted_hstates:
-            print("hstate {} is deleted".format(k))
-
-        return np.sum(n_deleted_hstateprops)
+        return n_deleted
 
     def _init_sufficient_statistics(self):
         sstats = super(FABPHMM, self)._init_sufficient_statistics()
         sstats["qsum_emit"] = np.zeros(self._n_hstates)
         sstats["qsum_trans"] = np.zeros(self._n_hstates)
-        sstats["norm_sum"] = 0
         return sstats
 
     def score(self, xseqs, yseqs, type="fic"):
@@ -283,13 +284,12 @@ class FABPHMM(PHMM):
         else:
             raise ValueError("invalid type")
 
-    def _accumulate_sufficient_statistics(self, sstats, free_energy, gamma, xi, xseq, yseq, norm):
+    def _accumulate_sufficient_statistics(self, sstats, free_energy, gamma, xi, xseq, yseq):
         # free energy is accumulated as sstats["score"]
         super(FABPHMM, self)._accumulate_sufficient_statistics(sstats, free_energy, gamma, xi, xseq, yseq)
         sum_gamma = np.sum(gamma, axis=(0, 1))
         sstats["qsum_emit"] += sum_gamma
         sstats["qsum_trans"] += sum_gamma - gamma[-1, -1, :]
-        sstats["norm_sum"] += np.sum(np.log(norm) * np.sum(gamma, axis=2))
 
     def _gen_random_qsum(self, xseqs, yseqs):
         n_seqs = len(xseqs)
@@ -307,9 +307,11 @@ class FABPHMM(PHMM):
         self._qsum_emit = sstats["qsum_emit"]
         self._qsum_trans = sstats["qsum_trans"]
         self._last_score = fic
+        self._check_probs()
 
     def _calculate_fic(self, sstats, n_seq):
         dim_init, dims_trans, dims_emit = self._gen_dims()
+
         fic = sstats["score"] \
               - dim_init / 2 * np.log(n_seq) \
               - np.sum(dims_trans / 2 * (np.log(sstats["qsum_trans"]) - 1)) \
@@ -317,31 +319,10 @@ class FABPHMM(PHMM):
         return fic
 
     def greedy_shrink(self):
-        if self._n_match_states == 1 and self._n_xins_states == 1 and self._n_yins_states == 1:
-            return False
+        return self._shrinkage_operation(force=True)
 
-        sep_mx = self._n_match_states
-        sep_xy = self._n_match_states + self._n_xins_states
+    def _delete_state(self, deleted_state):
 
-        qsum_emit_match = self._qsum_emit[:sep_mx]
-        qsum_emit_xins = self._qsum_emit[sep_mx:sep_xy]
-        qsum_emit_yins = self._qsum_emit[sep_xy:]
-
-        def min_val_index(qsum, origin):
-            argmin = np.argmin(qsum)
-            min_val = qsum[argmin] / np.sum(qsum)
-            min_index = argmin + origin
-            return min_val, min_index
-
-        cands = []
-        if self._n_match_states > 1:
-            cands.append(min_val_index(qsum_emit_match, 0))
-        if self._n_xins_states > 1:
-            cands.append(min_val_index(qsum_emit_xins, sep_mx))
-        if self._n_yins_states > 1:
-            cands.append(min_val_index(qsum_emit_yins, sep_xy))
-
-        _, deleted_state = min(cands, key=lambda k: k[0])
         preserved_hstates = np.delete(np.arange(self._n_hstates), deleted_state)
 
         deleted_hstateprop = self._hstate_properties[deleted_state]
@@ -364,23 +345,34 @@ class FABPHMM(PHMM):
         self._hstate_properties = self._gen_hstate_properties()
 
         self._initprob = self._initprob[preserved_hstates]
+        self._initprob /= np.sum(self._initprob)
         self._transprob = self._transprob[:, preserved_hstates]
         self._transprob = self._transprob[preserved_hstates, :]
+        self._transprob = self._transprob / np.sum(self._transprob, axis=1)[:, np.newaxis]
         self._emitprob = self._emitprob[preserved_hstates, :, :]
-        self._qsum_emit = self._qsum_emit[preserved_hstates]
-        self._qsum_trans = self._qsum_trans[preserved_hstates]
+        for k in range(self._n_hstates):
+            if self._hstate_properties[k] == 0:
+                self._emitprob[k] = self._emitprob[k] / np.sum(self._emitprob[k])
 
-        print("hstate {} is deleted".format(deleted_state))
+            if self._hstate_properties[k] == 1:
+                self._emitprob[k] = self._emitprob[k] / np.sum(self._emitprob[k], axis=0)[np.newaxis, :]
+
+            if self._hstate_properties[k] == 2:
+                self._emitprob[k] = self._emitprob[k] / np.sum(self._emitprob[k], axis=1)[:, np.newaxis]
+
+        self._qsum_emit = self._qsum_emit[preserved_hstates] + self._qsum_emit[deleted_state] / self._n_hstates
+        self._qsum_trans = self._qsum_trans[preserved_hstates] + self._qsum_trans[deleted_state] / self._n_hstates
+
+        self._check_probs()
 
         self._last_shrinked = True
-
-        return True
 
     def _compute_sstats(self, sstats, xseq, yseq, dims_trans, dims_emit, log_transprob, log_initprob, lock):
         log_emitprob_frame = self._gen_log_emitprob_frame(xseq, yseq)
 
-        norm, pseudo_prob = self._gen_pseudo_prob(log_emitprob_frame, dims_trans, dims_emit)
-        fab_log_emitprob_frame = log_emitprob_frame + log_(pseudo_prob * norm[:, :, np.newaxis])
+        log_delta = self._gen_logdelta(log_emitprob_frame, dims_trans, dims_emit)
+
+        fab_log_emitprob_frame = log_emitprob_frame + log_delta
 
         free_energy, fwd_lattice = self._forward(fab_log_emitprob_frame, log_transprob, log_initprob)
 
@@ -390,7 +382,7 @@ class FABPHMM(PHMM):
                                                      fab_log_emitprob_frame, log_transprob)
 
         with lock:
-            self._accumulate_sufficient_statistics(sstats, free_energy, gamma, xi, xseq, yseq, norm)
+            self._accumulate_sufficient_statistics(sstats, free_energy, gamma, xi, xseq, yseq)
 
     def fit(self, xseqs, yseqs, max_iter=1000, verbose=False, verbose_level=1, n_threads=4):
         if not self._params_valid:
@@ -398,7 +390,10 @@ class FABPHMM(PHMM):
             self._params_valid = True
 
         assert(len(xseqs) == len(yseqs))
+        assert(len(xseqs) > 0)
         n_seq = len(xseqs)
+
+        sys.stdout.flush()
 
         self._qsum_emit, self._qsum_trans = self._gen_random_qsum(xseqs, yseqs)
 
@@ -406,10 +401,13 @@ class FABPHMM(PHMM):
             self._last_score = - np.inf
 
         if self.monitor is None:
-            self.monitor = Monitor(threshold=self._shrink_threshold)
+            self.monitor = Monitor(threshold=self._stop_threshold)
 
-        for i in range(1, max_iter + 1):
+        i = 0
+        while i < max_iter:
+            i += 1
             print("{} th iter".format(i))
+
             log_transprob = log_(self._transprob)
             log_initprob = log_(self._initprob)
 
@@ -435,24 +433,28 @@ class FABPHMM(PHMM):
 
             fic = self._calculate_fic(sstats, n_seq)
 
-            self.monitor.write(fic, self._n_hstates, sstats)
+            self.monitor.write(fic / n_seq, self._n_hstates, sstats)
 
             if verbose:
                 self._print_states(i_iter=i, verbose_level=verbose_level)
+
+            self._update_params(sstats, fic)
 
             if self.monitor.is_converged():
                 print("converged with fic {}".format(fic))
                 return self
 
-            self._update_params(sstats, fic)
-
-            n_shrinked_states = self._shrinkage_operation(dry=(not self._shrink))
+            # dry is unavailable
+            n_shrinked_states = self._shrinkage_operation()
 
             # TODO: this part should be integrated into Monitor
             if n_shrinked_states > 0:
+                i = 0
                 if not self._shrink:
                     print("shrinked: not an optimal model")
                     return self
+
+            sys.stdout.flush()
 
         warnings.warn("end fitting though not yet converged", RuntimeWarning)
         return self
